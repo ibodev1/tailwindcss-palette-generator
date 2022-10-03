a3a668af62f728c0f0a3bb52a797849e8b7aadc9
ibodev1 <ibrahimodev42@gmail.com>
2022-10-03 23:44:28 +0300

    multiple value input added

--- example.js
+++ example.js
@@ -1,16 +1,26 @@
 const getPalette = require('./index');
 
-const palette = getPalette("#ffbd00", "primary", 500);
+const palette = getPalette([
+    {
+        color: "rgb(255, 189, 0)",
+        name: "primary",
+        shade: 400
+    },
+    {
+        color: "rgba(255, 189, 0, 1)",
+        name: "secondary",
+        shade: 500
+    },
+    {
+        color: "hsl(44, 100%, 50%)",
+        name: "tertiary",
+        shade: 600
+    },
+    {
+        color: "#FFBD00",
+        name: "quaternary",
+        shade: 50
+    }
+]);
 
-const primary = palette.palette;
-
-const paletteName = palette.name;
-
-const paletteDefaultColor = palette.defaultColor;
-
-const paletteMainShade = palette.mainShade;
-
-console.log(primary);
-console.log("Palette Name : " + paletteName);
-console.log("Palette Default Color : " + paletteDefaultColor);
-console.log("Palette Main Shade " + paletteMainShade);
+console.log(palette);
